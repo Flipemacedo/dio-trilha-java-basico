06669be9f9ecb853e6194840d7b40284bad83d9f
Flipemacedo <flipemacedo@hotmail.com>
2024-05-24 11:50:12 -0300

    Exercício finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("GOLIATH NATIONAL BANK");
+
+        System.out.print("Favor inserir o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Favor inserir o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Favor inserir o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Favor inserir o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+
+        String mensagemFinal = "Sua agência é " + agencia + ", conta " + numero + " e seu saldo "
+                + saldo + " ";
+
+        System.out.println(mensagemFinal);
+
+        scanner.close();
     }
 }
